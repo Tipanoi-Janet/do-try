1269624178dde8d9140b8256d29be474ea2e43e9
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-22 20:31:45 +0300

    first commit

---
+++ dotry.js
@@ -0,0 +1,39 @@
+let num=[3,65,9,56,43,32,12];
+for(let i =0; i<num.length; i++){
+    // console.log(i);
+    console.log(num[i]);
+
+}
+let items=[36,6,8,4,24,];
+for(let i=0; i<num.length; i++){
+    console.log(num[i]);
+} 
+let nums=[19,61,9,8,4,11,68];
+let k=0;
+do{
+    console.log(num[k]);
+    k++
+}
+while(k<num.length){
+
+
+
+let b= [3,4,5,6,7,8,98]
+let j=0;
+sum=0;
+multiply=2;
+console.log(num[j]);
+console.log(sum+=num[j]);
+console.log(multiply*=num[j]);
+j++
+}
+console.log({multiply});
+console.log({sum});{
+
+
+let b= [4,6,7,8,9,9]
+let c =0;
+modulus = 2;
+console.log(num=[c]);
+console.log(modulus%=num[c]);
+}
